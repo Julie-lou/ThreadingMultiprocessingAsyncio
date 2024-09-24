b5fbf8f1557212286f64916d41061c04840adb2c
Julie-lou <137873538+Julie-lou@users.noreply.github.com>
2024-09-24 13:23:43 +0700

    updated with numberfile.txt

--- generate_numbers.py
+++ generate_numbers.py
@@ -7,3 +7,5 @@ def generate_numbers_file(filename, num_numbers, min_value, max_value):
             number = random.randint(min_value, max_value)
             f.write(f"{number}\n")
     print(f"File '{filename}' with {num_numbers} random numbers generated.")
+
+print(generate_numbers_file("numberfile.txt", 1000, 0, 1000))
